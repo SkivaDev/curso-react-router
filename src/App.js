a8ef12af7d23c6ae7bac8631db059e2f54350836
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2022-10-25 20:29:40 -0500

    se agrego la seccion BlogPost usando useParams

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import BlogPage from "./BlogPage";
+import BlogPost from "./BlogPost";
 import HomePage from "./HomePage";
 import Menu from "./Menu";
 import ProfilePage from "./ProfilePage";
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/blog" element={<BlogPage />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="*" element={<p>Not found 404</p>} />
         </Routes>
