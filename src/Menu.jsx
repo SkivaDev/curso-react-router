877e90dd8beb6fb9882b176d78625f962409603d
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2022-10-28 17:55:12 -0500

    menu con rutas publicas y privadas

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "./auth";
 
 function Menu() {
+  const auth = useAuth();
+
   return (
     <nav>
       <ul>
-        {routes.map((route) => (
-          <li key={route.to}>
-            <NavLink
-              style={({ isActive }) => ({
-                color: isActive ? "red" : "blue",
-              })}
-              to={route.to}
-              end
-            >
-              {route.text}
-            </NavLink>
-          </li>
-        ))}
+        {routes.map((route) => {
+          if(route.publicOnly && auth.user) return null;
+          if(route.private && !auth.user) return null;
+
+
+          return (
+            <li key={route.to}>
+              <NavLink
+                style={({ isActive }) => ({
+                  color: isActive ? "red" : "blue",
+                })}
+                to={route.to}
+                end
+              >
+                {route.text}
+              </NavLink>
+            </li>
+          )
+        })}
       </ul>
     </nav>
   );
@@ -27,22 +36,28 @@ const routes = [];
 routes.push({
   to: "/",
   text: "Home",
+  private: false,
 });
 routes.push({
   to: "/blog",
   text: "Blog",
+  private: false,
 });
 routes.push({
   to: "/profile",
   text: "Profile",
+  private: true,
 });
 routes.push({
   to: "/login",
-  text: "LoginPage",
+  text: "Login",
+  private: false,
+  publicOnly: true,
 });
 routes.push({
   to: "/logout",
-  text: "LogoutPage",
+  text: "Logout",
+  private: true,
 });
 
 export default Menu;
